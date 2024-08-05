25f0fca0c3ed52236e12318fcfa1702bb0b3dd72
thisisabukars <abdabukarsh@gmail.com>
2024-08-05 20:16:43 +0300

    init header

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,10 +1,11 @@
 const Header = () => {
   return (
-    <div>
-      <header className="relative z-20 w-full top-0 bg-white shadow-md transition-transform">
-        i'm a header
-      </header>
-    </div>
+    <header className="relative z-20 w-full top-0 bg-white shadow-md transition-transform">
+      <div>{/* for head translation or something like that  */}</div>
+      <nav className="container mx-auto py-3 flex justify-between items-center lg:max-w-screen-dt ">
+        i'm a nav abr
+      </nav>
+    </header>
   );
 };
 
--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,10 +1,9 @@
 import dynamic from "next/dynamic";
 import Footer from "./Footer";
 import { lalezar, manrope, poppins, rubik } from "@/utils/font";
-import { Head } from "next/document";
 
 const Header = dynamic(() => import("./Header"), {
-  ssr: false, // Set to true if you want server-side rendering for this component
+  ssr: false,
 });
 
 const Layout = (props: any) => {
@@ -13,13 +12,6 @@ const Layout = (props: any) => {
       className={`${poppins.variable} ${lalezar.variable} ${manrope.variable} ltr:${manrope.className} rtl:${rubik.className} font-semibold relative z-0`}
     >
       <Header />
-      <Head>
-        <meta
-          charSet="utf-8"
-          name="viewport"
-          content="width=device-width, initial-scale=1.0"
-        />
-      </Head>
 
       <div className="bg-white">{props.children}</div>
 
