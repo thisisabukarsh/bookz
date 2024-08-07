0653df0e1fec9921bfa1e720dddb25e5a7823ce9
thisisabukars <abdabukarsh@gmail.com>
2024-08-07 11:29:57 +0300

    landing in home page done

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Book-Z</title>
+        <meta name="description" content="book-z description" />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,22 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Head from "next/head";
+import Landing from "../components/home/Landing";
+import { poppins, rubik } from "@/utils/font";
+import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <div> hello </div>;
+  return (
+    <div>
+      <Head>
+        <title>Book-Z</title>
+        <meta name="description" content="Home description" />
+      </Head>
+      <div className="w-full md:text-xs relative md:h-[550px]">
+        <Landing />
+      </div>
+    </div>
+  );
 }
