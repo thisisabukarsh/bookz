775d032cfac7e55b22b6299973da93eb0420f45c
thisisabukarsh <155810138+thisisabukarsh@users.noreply.github.com>
2024-08-13 00:18:51 +0300

    when login the buttons in landing disaper

--- src/components/home/landing/LoginSignUpButtons.tsx
+++ src/components/home/landing/LoginSignUpButtons.tsx
@@ -1,22 +1,44 @@
+import { RootState } from "@/lib/store";
 import Link from "next/link";
+import { useSelector } from "react-redux";
 
 const LoginSignUpButton: React.FC = () => {
-  return (
-    <div className="self-center flex flex-row mt-20">
-      <Link
-        href="/login"
-        className="min-w-28 inline-flex items-center justify-center bg-BookBlue text-white rounded-3xl shadow-md transition-all duration-300 ease-in-out text-lg font-medium h-12 px-6 py-2 mt-5 ml-2 hover:bg-[#003e7d] focus:outline-none focus:border-2 focus:border-blue-600 active:shadow-lg"
-      >
-        Login
-      </Link>
-      <Link
-        href="/signup"
-        className="inline-flex items-center justify-center bg-BookBlue text-white rounded-3xl shadow-md transition-all duration-300 ease-in-out text-lg font-medium h-12 px-6 py-2 mt-5 ml-2 hover:bg-[#003e7d] focus:outline-none focus:border-2 focus:border-blue-600 active:shadow-lg"
-      >
-        Sign Up
-      </Link>
-    </div>
-  );
+  const { isAuthenticated } = useSelector((state: RootState) => state.user);
+  if (!isAuthenticated) {
+    return (
+      <div className="self-center flex flex-row mt-20">
+        <Link
+          href="/login"
+          className="min-w-28 inline-flex items-center justify-center bg-BookBlue text-white rounded-3xl shadow-md transition-all duration-300 ease-in-out text-lg font-medium h-12 px-6 py-2 mt-5 ml-2 hover:bg-[#003e7d] focus:outline-none focus:border-2 focus:border-blue-600 active:shadow-lg"
+        >
+          Login
+        </Link>
+        <Link
+          href="/signup"
+          className="inline-flex items-center justify-center bg-BookBlue text-white rounded-3xl shadow-md transition-all duration-300 ease-in-out text-lg font-medium h-12 px-6 py-2 mt-5 ml-2 hover:bg-[#003e7d] focus:outline-none focus:border-2 focus:border-blue-600 active:shadow-lg"
+        >
+          Sign Up
+        </Link>
+      </div>
+    );
+  } else
+    return (
+      // <div className="self-center flex flex-row mt-20">
+      //   <Link
+      //     href="/profile"
+      //     className="inline-flex items-center justify-center bg-BookBlue text-white rounded-3xl shadow-md transition-all duration-300 ease-in-out text-lg font-medium h-12 px-6 py-2 mt-5 ml-2 hover:bg-[#003e7d] focus:outline-none focus:border-2 focus:border-blue-600 active:shadow-lg"
+      //   >
+      //     Profile
+      //   </Link>
+      //   <Link
+      //     href="/logout"
+      //     className="inline-flex items-center justify-center bg-BookBlue text-white rounded-3xl shadow-md transition-all duration-300 ease-in-out text-lg font-medium h-12 px-6 py-2 mt-5 ml-2 hover:bg-[#003e7d] focus:outline-none focus:border-2 focus:border-blue-600 active:shadow-lg"
+      //   >
+      //     Logout
+      //   </Link>
+      // </div>
+      <div></div>
+    );
 };
 
 export default LoginSignUpButton;
