ad30461bdb695290739984e81596622261309a5b
thisisabukarsh <155810138+thisisabukarsh@users.noreply.github.com>
2024-08-13 22:36:51 +0300

    fix responsive

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,6 +1,7 @@
 import dynamic from "next/dynamic";
 import Footer from "./Footer";
 import { lalezar, manrope, poppins, rubik } from "@/utils/font";
+import BottomBar from "./BottomBar";
 
 const Header = dynamic(() => import("./Header"), {
   ssr: false,
@@ -14,7 +15,10 @@ const Layout = (props: any) => {
       <Header parentClass="relative z-20 w-full top-0 bg-white shadow-md transition-transform" />
 
       <div className="bg-white">{props.children}</div>
-
+      <BottomBar
+        className={`lg:hidden fixed w-full bottom-0 bg-white pt-3 pb-5 z-10 ${poppins.className} text-xs `}
+        // } text-xs ${isDetailsPage ? "hidden" : ""}`}
+      />
       <Footer />
     </div>
   );
