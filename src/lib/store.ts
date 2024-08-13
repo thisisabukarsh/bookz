0881aeb33b7c816a22001bfb8540cd6dda5e7d14
thisisabukarsh <155810138+thisisabukarsh@users.noreply.github.com>
2024-08-13 19:02:03 +0300

    redux data done

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,15 +1,28 @@
-// import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import { useDispatch } from "react-redux";
-
-// redux/store.ts
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./slices/userSlice";
-import postsReducer from "./slices/postSlice";
+import postsReducer from "./slices/postsSlice";
+import loginReducer from "./slices/loginSlice";
+import signupReducer from "./slices/signupSlice";
+import resetPasswordReducer from "./slices/resetPasswordSlice";
+import fetchPostsReducer from "./slices/fetchPostsSlice";
+import fetchUserPostsReducer from "./slices/fetchUserPostsSlice";
+import createPostReducer from "./slices/createPostSlice";
+import ratingReducer from "./slices/ratingSlice";
+import profileReducer from "./slices/profileSlice";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     posts: postsReducer,
+    login: loginReducer,
+    signup: signupReducer,
+    resetPassword: resetPasswordReducer,
+    fetchPosts: fetchPostsReducer,
+    fetchUserPosts: fetchUserPostsReducer,
+    createPost: createPostReducer,
+    rating: ratingReducer,
+    profile: profileReducer,
+    DeletePost: postsReducer,
   },
 });
 
